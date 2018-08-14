5763f9060a4e8fbd080fcbf6d7aa9ea5987d66f7
Ansel Adams <dj@volaciousbrands.com>
2018-08-14 12:28:46 -0500

    styling, API links

--- controllers/burger-controller.js
+++ controllers/burger-controller.js
@@ -16,6 +16,36 @@ module.exports = function (app) {
             res.render("index", hbsObj);
         });
     });
+    app.get("/api/burgers/", function (req, res) {
+        console.log("got here");
+        db.Burger.findAll({
+            include: [{model: db.User}]
+        }).then(function (data) {
+            res.json(data);
+        });
+    });
+    app.get("/api/burgers/menu", function (req, res) {
+        console.log("got here");
+        db.Burger.findAll({
+            where: {
+                devoured: false
+            },
+            include: [{model: db.User}]
+        }).then(function (data) {
+            res.json(data);
+        });
+    });
+    app.get("/api/burgers/devoured", function (req, res) {
+        console.log("got here");
+        db.Burger.findAll({
+            where: {
+                devoured: true
+            },
+            include: [{model: db.User}]
+        }).then(function (data) {
+            res.json(data);
+        });
+    });
 
     // POST route for saving a new todo. We can create a todo using the data on req.body
     app.post("/api/burgers", function (req, res) {
@@ -23,6 +53,7 @@ module.exports = function (app) {
         console.log("REQ BODY: ", req.body);
         db.Burger.create({
             burger_name: req.body.burger_name,
+            devoured: false
         }).then(function (result) {
             res.json(result);
         })
